7e0f13c8c2ef43c4f4645e5aaa207bf4181c7463
IzaakCod <zaaknascimentosantos@gmail.com>
2026-07-22 18:24:59 -0300

    Vimos nessas aula um pouco de let vs var, e qual a diferença entre os dois. E tipos dados primitivos

---
+++ Secao2-jsbasico-para-iniciantes/aula9-let-vs-var_primieras-diferencas/index.js
@@ -0,0 +1,4 @@
+let nome = "Izaak" //let não permite que a variável possa ser redeclarada
+
+var nome = "Izaak" // var permite que a variável possa ser redeclarada
+console.log(nome)
---
+++ Secao2-jsbasico-para-iniciantes/aula9a-tipos-de-dados-primitivos/tipos-dados-primitivos.js
@@ -0,0 +1,11 @@
+// String, Number, Undefined, Null, Boolean, symbol
+const nome = "Izaak" // String
+const nome1 = 'Izaak' // String
+const nome2 = `Izaak` // String
+const num1 = 10 // Number
+const num2 = 10.25 // Number
+let nomeAluno // Underfined -> não aponta pra local nenhuma na memória
+const sobrenomeAluno = null // Nulo -> não aponta pra local nenhuma na memóriaff
+const aprovado = false // Boolean = true, false (lógico)
+
+console.log(typeof aprovado, aprovado)
