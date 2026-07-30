efa8c812e277d3f480f533b68e249f2c9fad7fd7
IzaakCod <zaaknascimentosantos@gmail.com>
2026-07-30 17:32:25 -0300

    Exercicío de soma de dois números!!!

---
+++ Secao2-jsbasico-para-iniciantes/aula11-alert-confirm-prompt-(navegador)/js/script.js
@@ -0,0 +1,13 @@
+let num1
+let num2
+
+//num1 = Number(num1)
+//num2 = Number(num2)
+
+num1 = parseInt(prompt("Digite o primeiro número: "))
+num2 = parseInt(prompt("Digite o segundo número:"))
+
+const soma = num1 + num2
+
+alert("O resultado da soma dos números " +num1+ " e " +num2+ " é " +soma)
+alert(`O resultado é soma dos números é: ${soma}`)
