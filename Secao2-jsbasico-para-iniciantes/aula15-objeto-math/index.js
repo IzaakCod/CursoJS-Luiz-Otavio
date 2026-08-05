9c9e5c3a94642e3e3cde0ef9e703153a9e96479a
IzaakCod <zaaknascimentosantos@gmail.com>
2026-08-05 17:24:37 -0300

    Aula 15, vendo um pouco sobre objetos Math

---
+++ Secao2-jsbasico-para-iniciantes/aula15-objeto-math/index.js
@@ -0,0 +1,13 @@
+console.log()
+
+
+// let num2 = Math.floor(num1) // Arredonda o valor ou número para baixo
+// let num2 = Math.ceil(num1) // Arrendonda o valor ou número para cima 
+// let num2 = Math.round(num1) // Arrendonda o valor ou número para o mais próximo dele Ex: 9.51 = 10
+// console.log(Math.max(1, 2, 3, 8, 9, 52, 683, 846, 924 )) Dar o maior número de uma lista de números
+// console.log(Math.min(1, 2, 3, 8, 9, 52, 683, 846, 924 )) Dar o menor número de uma lista de números
+// console.log(Math.random()) Gerar números aleatórios com várias casas decimais
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+// console.log(Math.PI) Dar o valor de PI
+// console.log(Math.pow(2,10)) Dar a pontêcia do número desejado
+// console.log(num1 ** 0.5) Assim descobri a raiz quadrada do número
