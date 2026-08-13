d2dadad25d7257844c9c2b7693208be1f3ac191f
IzaakCod <zaaknascimentosantos@gmail.com>
2026-08-13 16:50:29 -0300

    Aula 16 vendo o básico de Array

---
+++ Secao2-jsbasico-para-iniciantes/aula16-arrays(basico)/index.js
@@ -0,0 +1,44 @@
+//            01234
+const nome = "Izaak"
+// console.log(nome[5])
+
+
+//               0        1        2            3       4
+let alunos = ["Izaak", "Davi", "Flávia", ] // Luiza, Eduardo
+alunos = 13235
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+
+
+
+// console.log(alunos)
+// console.log(alunos[0])
+// console.log(alunos[2]) Como acessar qual quer valor do Array 
+
+
+// alunos[alunos.length] = "Kauê" Adiciona valor no final do Array
+// alunos[alunos.length] = "Larisse"
+// alunos[alunos.length] = "Luan" 
+
+
+// alunos[0] = "Djalma" Altera
+// alunos[2] = "Leila"  Adiciona 
+
+// alunos.push("Lavínia") // Adiciona valor no final do Array*/
+
+// alunos.unshift("Bob") Adiciona no começo do Array  */
+
+// alunos.pop() //Remove o ultimo elemento do Array 
+// const removido = alunos.pop
+// console.log(removido) // E essa varíavel mostra o elemento que foi removido
+
+
+// alunos.shift() Remove o primeiro elemento, remove o índice 0, e o nome seguinte passa a ser o índice 0
+
+// delete alunos[1] Assim os índices não podem ser alterados, só será removido o elemento 
+
+// console.log(alunos[6]) Assim posso acessar um elemento que não existe, e irá aparecer undefined
+
+// console.log(alunos.slice(0, 3)) Pega os elementos dos números que vc digitou
