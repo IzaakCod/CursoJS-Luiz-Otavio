c72af249565ffb1e6ffea8449fd3d6a010269ffb
IzaakCod <zaaknascimentosantos@gmail.com>
2026-08-18 16:49:21 -0300

    Aula 17 sobre o básico de funções

---
+++ Secao2-jsbasico-para-iniciantes/aula17-funcoes(basico)/index.js
@@ -0,0 +1,11 @@
+/* function soma(x = 5, y = 5){
+    const resultado = x + y
+    return resultado
+}
+const resultado = (soma(4, 6))
+console.log(resultado) */
+
+const raiz = function(n){
+    return n ** 0.5
+}
+console.log(raiz(9))
