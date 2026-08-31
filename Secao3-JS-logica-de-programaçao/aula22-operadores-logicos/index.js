fbe4059a3fb1aeebc905b751b6283d438ffa3d0c
IzaakCod <zaaknascimentosantos@gmail.com>
2026-08-31 16:50:20 -0300

    Aula 22 sobre operadores lógicos!!!

---
+++ Secao3-JS-logica-de-programaçao/aula22-operadores-logicos/index.js
@@ -0,0 +1,21 @@
+/*  
+OPERADORES LÓGICOS
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU ->
+! -> NOT -> NÃO ->
+
+*/
+
+// const expressaoAnd = true && true && true && true
+
+// const expressaoOr = false || false || true || false
+
+// const usuario = "Izaak" // Form que usuário digitou
+// const senha = "123456" // Form que usuário digitou
+
+//                      True                  False
+// const vaiLogar = usuario === "Izaak" && senha === "12345"
+
+
+
+console.log(false)
