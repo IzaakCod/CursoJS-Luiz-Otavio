bb4d75443834eb99571c69a8771b828956694b15
IzaakCod <zaaknascimentosantos@gmail.com>
2026-09-18 15:40:49 -0300

    Conclusão do exercício!!!!

--- Secao3-JS-logica-de-programaçao/aula26-exercicio(tudo que aprendemos ate agora)/main.js
+++ Secao3-JS-logica-de-programaçao/aula26-exercicio(tudo que aprendemos ate agora)/main.js
@@ -8,47 +8,62 @@ form.addEventListener("submit", function (e) {
     const inputPeso = e.target.querySelector("#peso")
     const inputAltura = e.target.querySelector("#altura")
 
-const peso = Number(inputPeso.value)
-const altura = Number(inputAltura.value)
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
 
-console.log(peso, altura)
+    if (!peso) {
+        setResultado("Peso inválido", false)
+        return
+    }
+    if (!altura) {
+        setResultado("Altura inválida", false)
+        return
+    }
+
+    const imc = getImc(peso, altura)
+    const nivelImc = getNivelImc(imc)
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}) `
+
+    setResultado(msg, true)
 
 })
+
+function getNivelImc(imc) {
+    const nivel = ["Abaixo do peso", "Peso normal", "Sobrepeso",
+        "Obesidade grau 1", "Obesidade grau 2", "Obesidade grau 3"]
+ 
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+}
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+}
 
 
 function criaP() {
     const p = document.createElement("p")
-    return p 
+    return p
 }
 
 
-function setResultado(msg) {
+function setResultado(msg, isValid) {
     const resultado = document.querySelector("#resultado")
     resultado.innerHTML = ""
+
     const p = criaP()
 
-
+    if (isValid) {
+        p.classList.add("paragrafo-resultado")
+    } else {
+        p.classList.add("bad")
+    }
+    p.innerHTML = msg
     resultado.appendChild(p)
 }
-
-/* let peso = Number(prompt("Qual o seu peso: "))
-let altura = Number(prompt("Qual a sua altura: "))
-
-let imc = peso / (altura * altura)
-if(imc < 18,5){
-}
-else if(imc > 18,5 && imc < 24,9){
-
-}
-else if(imc > 25 &&  imc < 29,9){
-    
-}
-else if(imc > 30 && imc < 34,9){
-
-}
-else if(imc > 35 && imc < 39,9){
-
-}
-else{
-
-} */
